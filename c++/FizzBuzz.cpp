3d84fcf272b9194096da664808e2494cf1ea67a1
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2014-03-15 21:58:00 +0100

    use std namespace

--- c++/FizzBuzz.cpp
+++ c++/FizzBuzz.cpp
@@ -2,13 +2,16 @@
 #include <sstream>
 #include "FizzBuzz.h"
 
-std::string FizzBuzz::turn(int number)
-{
-    if (0 == number % 3 && 0 == number % 5) { return std::string("FizzBuzz"); }
-    if (0 == number % 3) { return std::string("Fizz"); }
-    if (0 == number % 5) { return std::string("Buzz"); }
+using namespace std;
 
-    std::ostringstream oss;
+string FizzBuzz::turn(int number)
+{
+    if (0 == number % 3 && 0 == number % 5) { return string("FizzBuzz"); }
+    if (0 == number % 3) { return string("Fizz"); }
+    if (0 == number % 5) { return string("Buzz"); }
+
+    ostringstream oss;
     oss << number;
+
     return oss.str();
 }
