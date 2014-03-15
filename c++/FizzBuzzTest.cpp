f76633cf572808af8c4d9c40aa856e54bec60e67
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2014-03-15 21:35:32 +0100

    use std namespace

--- c++/FizzBuzzTest.cpp
+++ c++/FizzBuzzTest.cpp
@@ -2,49 +2,46 @@
 #include <vector>
 #include "FizzBuzz.h"
 
-typedef std::pair<int, std::string> Expectation;
+using namespace std;
+
+typedef pair<int, string> Expectation;
 
 int main()
 {
-    std::vector<Expectation> expectations;
-    expectations.push_back(std::make_pair(1, "1"));
-    expectations.push_back(std::make_pair(2, "2"));
-    expectations.push_back(std::make_pair(3, "Fizz"));
-    expectations.push_back(std::make_pair(4, "4"));
-    expectations.push_back(std::make_pair(5, "Buzz"));
-    expectations.push_back(std::make_pair(6, "Fizz"));
-    expectations.push_back(std::make_pair(10, "Buzz"));
-    expectations.push_back(std::make_pair(14, "14"));
-    expectations.push_back(std::make_pair(15, "FizzBuzz"));
-    expectations.push_back(std::make_pair(29, "29"));
-    expectations.push_back(std::make_pair(30, "FizzBuzz"));
-    expectations.push_back(std::make_pair(31, "31"));
-    expectations.push_back(std::make_pair(33, "Fizz"));
+    vector<Expectation> expectations;
+    expectations.push_back(make_pair(1, "1"));
+    expectations.push_back(make_pair(2, "2"));
+    expectations.push_back(make_pair(3, "Fizz"));
+    expectations.push_back(make_pair(4, "4"));
+    expectations.push_back(make_pair(5, "Buzz"));
+    expectations.push_back(make_pair(6, "Fizz"));
+    expectations.push_back(make_pair(10, "Buzz"));
+    expectations.push_back(make_pair(14, "14"));
+    expectations.push_back(make_pair(15, "FizzBuzz"));
+    expectations.push_back(make_pair(29, "29"));
+    expectations.push_back(make_pair(30, "FizzBuzz"));
+    expectations.push_back(make_pair(31, "31"));
+    expectations.push_back(make_pair(33, "Fizz"));
 
-    std::string result;
+    string result;
 
-    for (std::vector<Expectation>::iterator expectation = expectations.begin();
+    for (vector<Expectation>::iterator expectation = expectations.begin();
             expectation != expectations.end();
             ++expectation
     ) {
         result = FizzBuzz::turn(expectation->first);
 
         if (0 == result.compare(expectation->second)) {
-            std::cout << ".";
+            cout << ".";
         }
         else {
-            std::cout << std::endl;
-            std::cout << "Failure: ";
-            std::cout << result;
-            std::cout << " does not equal ";
-            std::cout << expectation->second;
-            std::cout << std::endl;
+            cout << endl << "Failure: " << result << " does not equal " << expectation->second << endl;
 
             return 1;
         }
     }
 
-    std::cout << std::endl << "Tests complete. No failures" << std::endl;
+    cout << endl << "Tests complete. No failures" << endl;
 
     return 0;
 }
