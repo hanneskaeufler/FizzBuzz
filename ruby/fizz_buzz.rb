ea52aa7c16e4b8fbda0bd1834ab7d954dde27f0d
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2014-03-15 15:51:07 +0100

    add ruby implementation

--- ruby/fizz_buzz.rb
+++ ruby/fizz_buzz.rb
@@ -1,3 +1,9 @@
 class FizzBuzz
+    def self.turn(number)
+        return "FizzBuzz" if number.modulo(3).zero? && number.modulo(5).zero?
+        return "Fizz" if number.modulo(3).zero?
+        return "Buzz" if number.modulo(5).zero?
 
+        number
+    end
 end
--- ruby/fizz_buzz_spec.rb
+++ ruby/fizz_buzz_spec.rb
@@ -2,5 +2,25 @@ require "minitest/autorun"
 require_relative "fizz_buzz"
 
 describe FizzBuzz do
+    it "returns 1 for 1" do
+        cases = [
+            [1, 1],
+            [2, 2],
+            [3, "Fizz"],
+            [4, 4],
+            [5, "Buzz"],
+            [6, "Fizz"],
+            [10, "Buzz"],
+            [14, 14],
+            [15, "FizzBuzz"],
+            [29, 29],
+            [30, "FizzBuzz"],
+            [31, 31],
+            [33, "Fizz"],
+        ]
 
+        cases.each do |theCase|
+            FizzBuzz.turn(theCase[0]).must_equal(theCase[1])
+        end
+    end
 end
